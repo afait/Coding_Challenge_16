c71c34f1ace89f886f729dc21d6b9e162d15864e
Annabelle Fait <imannaf8@gmail.com>
2025-03-30 22:20:46 -0400

    Task 3 - Add fetchProductsAsync using async/await and try/catch

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -1,3 +1,5 @@
+// Taks 2 - Fetch Products with .then()
+
 function fetchProductsThen() {
     fetch('https://www.course-api.com/javascript-store-products')
     // Requests the URL
@@ -18,4 +20,45 @@ function fetchProductsThen() {
             console.error('An Error Has Occured:', error);
         });
         // if there is an error, it will log "An Error Has Occured" with the name of the error
-}
+};
+
+
+// Task 3 - Fetch Products with async/await
+
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch('https://www.course-api.com/javascript-store-products')
+        // fetches the website
+        if (!response.ok) {
+            throw new Error ('Error with network');
+        }
+        // If there is an error connecting the console will show "Error with network"
+        const products = await response.json();
+        displayProducts(products);
+        // Displays all of teh products
+    } catch (error) {
+        handleError(error)
+    };
+    // If an error occurs, it gets passed to handleError
+    function displayProducts(products) {
+        const productList = document.getElementById('list of products');
+// Created a function to show all of the products
+
+        products.forEach(product => {
+            const productElement = document.createElement('div');
+            productElement.classList.add('product');
+            productElement.textContent = product.name; 
+            productList.appendChild(productElement);
+          });
+          // Appends the previous product list
+        }
+        
+        function handleError(error) {
+          console.error('Error fetching products:', error);
+          // If there is an error it gets passed to this handleError which will log "Error fetching products" and display the name of the error that occurred
+          const errorElement = document.createElement('div');
+          errorElement.classList.add('error');
+          errorElement.textContent = 'Failed to load products. Please try again later.';
+          document.body.appendChild(errorElement);
+        }
+    };
